494636707844c0553ce2f7afb438fa04dea72d55
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-16 05:43:22 +0300

    modifying pages + scss
    creating separate folders for all the pages and change to scss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import './App.css';
 import Layout from './components/Layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
+
   return (
     <Layout/>
   );
---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Admin.module.scss'
+const Admin = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import styles from './Admin.module.scss'
+export default Admin
