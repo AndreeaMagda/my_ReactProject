4c6024311f57ff06e207bdd7a0570a3650d93e8c
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-16 06:07:58 +0300

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 
-import './App.css';
-import Layout from './components/Layout/Layout';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from "./pages/home/Home";
+import Books from "./pages/books/Books";
+import Movies from "./pages/movies/Movies";
+import PlaceToVisit from "./pages/placeToVisit/Places";
+import Quiz from "./pages/quiz/Quiz";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
+import Account from './pages/account/Account';
+import Favorite from './pages/favorite/Favorite';
+import  Header  from './components/header/Header';
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
-    <Layout/>
+    <>
+  
+    <Header/>
+      <Routes>
+         <Route path='home' element={<Home />}/>
+       
+        <Route path='books' element={<Books />}/>
+       
+        <Route path='movies' element={<Movies />}/>
+        <Route path='place' element={<PlaceToVisit />}/>
+        <Route path='quiz' element={<Quiz/>}/>
+        <Route path='shop' element={<Shop />}/>
+        <Route path='cart' element={<Cart/>}/>
+        <Route path='favorite' element={<Favorite />}/>
+        <Route path='account' element={<Account/>}/>
+      </Routes>
+    <Footer/>
+   
+    </>
   );
+  
 }
 
 export default App;
---
+++ src/pages/oderHistory/oderHistory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const oderHistory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default oderHistory
