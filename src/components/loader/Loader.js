1a7b539d8b2ce8a8e7e9d82f15f08136f4c4e1e2
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-20 08:31:30 +0300

    Loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Loader'
+import loadGif from '../../Images/loader.gif'
+import { ReactDOM } from 'react-dom'
+const Loader = () => {
+  return ReactDOM.createPortal (
+    <div className={styles.wrapper}>
+        <div className={styles.loader}>
+            <img src={loadGif} alt='err loading gif'/>
+
+        </div>
+      
+    </div>,
+    document.getElementById("loader")
+  )
+}
+
+export default Loader
