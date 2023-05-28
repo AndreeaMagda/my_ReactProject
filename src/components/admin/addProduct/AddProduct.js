a00de679816662fc75a76a4b3093c45c33c65273
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-28 16:08:43 +0300

    Update AddProduct.js
    upload some products to firebase

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -4,13 +4,14 @@ import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { db, storage } from '../../../firebase/config';
 import { toast } from 'react-toastify';
 import { Timestamp, addDoc, collection } from 'firebase/firestore';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Loader } from 'semantic-ui-react';
 
 const categories = [
-  { id: 1, name: "Chlotes" },
+  { id: 1, name: "Clothes" },
   { id: 2, name: "Toys" },
   { id: 3, name: "Jewelry" },
+  { id: 3, name: "Wands" },
 
 ];
 
@@ -19,13 +20,14 @@ const initialState = {
   imageURL: "",
   price: 0,
   category: "",
- 
+  desc: "",
+
 };
 const AddProduct = () => {
   const [product, setProduct] = useState(
     {
-     ...initialState
-  
+      ...initialState
+
     }
   )
 
@@ -72,14 +74,15 @@ const AddProduct = () => {
 
   const addProduct = (e) => {
     e.preventDefault();
-   // console.log(product);
-      setIsLoading(true);
+    // console.log(product);
+    setIsLoading(true);
     try {
       const docRef = addDoc(collection(db, "products"), {
         name: product.name,
         imageURL: product.imageURL,
         price: Number(product.price),
         category: product.category,
+        desc: product.desc,
         createdAt: Timestamp.now().toDate(),
       });
       setIsLoading(false);
@@ -96,67 +99,77 @@ const AddProduct = () => {
 
   return (
     <>
-    {isLoading &&<Loader/>}
-    <div>
+      {isLoading && <Loader />}
+      <div>
 
-      <h1>Add new product</h1>
+        <h1>Add new product</h1>
 
-      <form onSubmit={addProduct}>
-        <div className="form-row">
-          <div className="col-md-4 ">
-            <label>Insert product name</label>
-            <input type="text" className="form-control" placeholder="Product name"
-              required name='name'
-              value={product.name}
-              onChange={(e) => handleInputChange(e)} />
-            <br />
-
-
-            <label>Choose product image</label>
-            <div className="custom-file">
-              <input type="file" accept="image/*" className="custom-file-input" id="validatedCustomFile"
-                name="image" onChange={(e) => handleImageChange(e)} />
-
-
-              <input type="text" className="form-control" placeholder="image URL"
-                // required
-                name='imageURL' value={product.imageURL}
+        <form onSubmit={addProduct}>
+          <div className="form-row">
+            <div className="col-md-4 ">
+              <label>Insert product name</label>
+              <input type="text" className="form-control" placeholder="Product name"
+                required name='name'
+                value={product.name}
                 onChange={(e) => handleInputChange(e)} />
-              <label className="custom-file-label" htmlFor="validatedCustomFile">Choose file...</label>
-              <div className="invalid-feedback">Example invalid custom file feedback</div>
+              <br />
+
+
+              <label>Choose product image</label>
+              <div className="custom-file">
+                <input type="file" accept="image/*" className="custom-file-input" id="validatedCustomFile"
+                  name="image" onChange={(e) => handleImageChange(e)} />
+
+
+                <input type="text" className="form-control" placeholder="image URL"
+                  // required
+                  name='imageURL' value={product.imageURL}
+                  onChange={(e) => handleInputChange(e)} />
+                <label className="custom-file-label" htmlFor="validatedCustomFile">Choose file...</label>
+                <div className="invalid-feedback">Example invalid custom file feedback</div>
+              </div>
+
+
+              <br />  <br />
+              <label>Insert product pice</label>
+              <input type="number" className="form-control" placeholder="Product price"
+                required name='price' value={product.price}
+                onChange={(e) => handleInputChange(e)} />
+              <br />
+
+
+              <label>Select product category</label>
+              <select required name='category' value={product.category} onChange={(e) => handleInputChange(e)} className="custom-select">
+                <option selected>Select</option>
+                {categories.map((cat) => {
+                  return (
+                    <option key={cat.id} value={cat.name}>
+                      {cat.name}
+                    </option>
+                  );
+                })}
+              </select> <br /><br />
+              <div class="input-group">
+
+                <div class="input-group-prepend">
+                  <span class="input-group-text">Description </span>
+                </div>
+                <textarea class="form-control" aria-label="With textarea"
+                  name="desc"
+                  required
+                  value={product.desc}
+                  onChange={(e) => handleInputChange(e)}></textarea>
+              </div>
+
+              <br /> <br />
+              <button type="submit" className="btn btn-success">Add product</button>
+
             </div>
-
-
-            <br />  <br />
-            <label>Insert product pice</label>
-            <input type="number" className="form-control" placeholder="Product price"
-              required name='price' value={product.price}
-              onChange={(e) => handleInputChange(e)} />
-            <br />
-
-
-            <label>Select product category</label>
-            <select required name='category' value={product.category} onChange={(e) => handleInputChange(e)} className="custom-select">
-              <option selected>Select</option>
-              {categories.map((cat) => {
-                return (
-                  <option key={cat.id} value={cat.name}>
-                    {cat.name}
-                  </option>
-                );
-              })}
-            </select>
-
-
-            <br /> <br />
-            <button type="submit" className="btn btn-success">Add product</button>
 
           </div>
 
-        </div>
-
-      </form>
-    </div>
+        </form>
+      </div>
     </>
   )
 }
