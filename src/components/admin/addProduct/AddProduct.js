d9eec9342421e08889f949096179c74364f214d9
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-28 11:44:26 +0300

    Update AddProduct.js

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -2,35 +2,73 @@ import { useState } from 'react'
 import Navbar from '../navbar/Navbar';
 
 const AddProduct = () => {
-
- const [product, setProduct] = useState(
+  const categories = [
+    { id: 1, name: "Chlotes" },
+    { id: 2, name: "Toys" },
+    { id: 3, name: "Jewelry" },
+ 
+  ];
+  const [product, setProduct] = useState(
     {
       name: "",
-      imageUrl: "",
+      imageURL: "",
       price: null,
       category: "",
-      brand: ""
+
     }
   )
 
-  const handleInputChange=(e)=>{};
-  const handleImageChange=(e)=>{};
+  const handleInputChange = (e) => { };
+  const handleImageChange = (e) => { };
 
   return (
     <div>
-     
-     <h1>Add new product</h1> 
 
-     <form>
-     <div className="form-row">
-    <div className="col-md-4 ">
-      <input type="text" class="form-control" placeholder="First name"
-    required name='name' value={product.name}
-    onChange={(e)=>handleInputChange(e)}/>
-  </div>
-  </div>
- 
-</form>
+      <h1>Add new product</h1>
+
+      <form>
+        <div className="form-row">
+          <div className="col-md-4 ">
+            <label>Insert product name</label>
+            <input type="text" class="form-control" placeholder="Product name"
+              required name='name' value={product.name}
+              onChange={(e) => handleInputChange(e)} />
+            <br />
+            <label>Choose product image</label>
+
+            <div class="custom-file">
+              <input type="file" accept="image/*" class="custom-file-input" id="validatedCustomFile"
+                name="image" onChange={(e) => handleImageChange(e)} />
+              <input type="text" class="form-control" placeholder="image URL"
+                required name='imageURL' value={product.imageURL}
+                onChange={(e) => handleInputChange(e)} />
+              <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+              <div class="invalid-feedback">Example invalid custom file feedback</div>
+            </div>
+            <br />  <br />
+            <label>Insert product pice</label>
+            <input type="number" class="form-control" placeholder="Product price"
+              required name='price' value={product.price}
+              onChange={(e) => handleInputChange(e)} />
+            <br />
+            <label>Select product category</label>
+            <select required name='category' value={product.category} onChange={(e) => handleInputChange(e)} class="custom-select">
+              <option selected>Select</option>
+              {categories.map((cat) => {
+                return (
+                  <option key={cat.id} value={cat.name}>
+                    {cat.name}
+                  </option>
+                );
+              })}
+            </select>
+            <br /> <br />
+            <button type="button" class="btn btn-success">Add product</button>
+          </div>
+
+        </div>
+
+      </form>
     </div>
   )
 }
